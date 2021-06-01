b9fa3d1492b7e4fa1cc7fe2304e589bf57be96d9
Adelaide <teofani.adelaide@gmail.com>
2021-06-01 11:35:49 +0200

    Delete install.R

--- install.R
+++
@@ -1,8 +0,0 @@
-install.packages("tidyverse")
-install.packages('vegan')
-install.packages('ggplot2')
-install.packages('plotly')
-install.packages('reshape2')
-install.packages('plyr')
-conda install -c bioconda bioconductor-deseq2
-conda install -c bioconda bioconductor-phyloseq
