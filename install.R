ab11cf6f8512395d4bc1547a86d3db1b95a7d049
Adelaide <teofani.adelaide@gmail.com>
2021-06-01 09:55:01 +0200

    add yml

--- install.R
+++ install.R
@@ -2,7 +2,7 @@ install.packages("tidyverse")
 install.packages('vegan')
 install.packages('ggplot2')
 install.packages('plotly')
-install.packages('DESeq2')
-install.packages('phyloseq')
 install.packages('reshape2')
 install.packages('plyr')
+conda install -c bioconda bioconductor-deseq2
+conda install -c bioconda bioconductor-phyloseq
